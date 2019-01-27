1bde1507ac9da4541a26efb72210dc8939d8212e
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 01:47:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,5 @@ def square_array(array)
   # your code here
   numbers = []
 
-  numbers.collect {
-    array.each do |i|
-    numbers.push(i**2)
-  }
-  end
-  return numbers
+  numbers.collect {i**2}
 end
